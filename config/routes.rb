79749af551e5d643dd41e5ffa149ac01c916e028
dlewis39 <dlewis39@gmail.com>
2017-02-16 21:06:00 -0500

    Added a blog landing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   resources :blogs
-  get 'pages/aboutus'
-  get 'pages/contactus'
 
   get 'welcome/homepage'
 
@@ -9,12 +7,15 @@ Rails.application.routes.draw do
   resources :posts, expect: [:show, :index, :edit] do
   	resources :comments
   end
-  
 
   root 'welcome#homepage'
   resources :blogs
 
   get 'pages/aboutus' => 'pages#aboutus'
+  get 'pages/contactus' => 'pages#contactus'
+
   get 'posts' => 'posts#index'
   get 'posts/:id' => 'posts#show'
+
+  get 'blogs' => 'blogs#index'
 end
