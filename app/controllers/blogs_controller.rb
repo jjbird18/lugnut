4f62a1148c406a44a960562691fb4ffc1b32885e
dlewis39 <dlewis39@gmail.com>
2017-03-04 17:47:43 -0500

    Added AWS S3 for image hosting

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -74,6 +74,6 @@ class BlogsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def blog_params
-      params.require(:blog).permit(:title, :description, :main_content, :image, :user_id)
+      params.require(:blog).permit(:title, :description, :main_content, :image_file_name, :user_id)
     end
 end
